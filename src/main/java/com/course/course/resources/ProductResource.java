f8247cfa3ffe0b1e3289b434b38d2d569dbf31c9
João Pedro <joaopedroorem@gmail.com>
2026-04-13 20:12:24 -0300

    Criação entidade Product

--- src/main/java/com/course/course/resources/ProductResource.java
+++ src/main/java/com/course/course/resources/ProductResource.java
@@ -1,7 +1,7 @@
 package com.course.course.resources;
 
-import com.course.course.entities.Order;
-import com.course.course.services.OrderService;
+import com.course.course.entities.Product;
+import com.course.course.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,22 +12,21 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/orders")
-public class OrderResource {
+@RequestMapping(value = "/products")
+public class ProductResource {
 
     @Autowired
-    private OrderService service;
+    private ProductService service;
 
     @GetMapping
-    public ResponseEntity<List<Order>> findAll(){
-        List<Order> list = service.findAll();
+    public ResponseEntity<List<Product>> findAll(){
+        List<Product> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<Order> findById(@PathVariable Long id){
-        Order obj = service.findById(id);
+    public ResponseEntity<Product> findById(@PathVariable Long id){
+        Product obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
     }
-
 }
